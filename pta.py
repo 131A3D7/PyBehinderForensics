89a0646b30e5dc7fef06bbdc45cde971cf84fb2b
传铭 陈 <legendchen131a3d7@outlook.com>
2023-12-01 20:06:53 +0800

    修改了代码，原本传错了

--- pta.py
+++ pta.py
@@ -6,11 +6,14 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 import binascii
 import json
+
+# 解析命令行参数
 parser = argparse.ArgumentParser(description='冰蝎快速解析脚本')
 parser.add_argument('-k', '--key', help='冰蝎解密的key, 默认是e45密钥',
                     required=False, default='e45e329feb5d925b')
 parser.add_argument('-f', '--file', help='文件位置', required=True)
-parser.add_argument('-d', '--decrypt', help='被加密的冰蝎shell', required=True)
+parser.add_argument('-d', '--decrypt', help='被加密的冰蝎shell文件', required=False)
+parser.add_argument('-l', '--list', help='需要批量解密的目录', required=False)
 args = parser.parse_args()
 
 # 定义可能存在的key的正则表达式
@@ -19,11 +22,8 @@ maybe_aeskey = re.compile(r'[a-fA-F0-9]{16}')
 # 打开文件并读取内容
 with open(args.file, 'r', encoding='utf-8') as f:
     content = f.read()
-    f.close
 
 # 定义寻找key的函数
-
-
 def printmaybekey(check_key):
     if any(key != args.key for key in check_key):
         print('疑似检测到非默认密钥, 可能的key列表如下:')
@@ -31,62 +31,74 @@ def printmaybekey(check_key):
             print(f'{i}. {key}')
         print('请选择你想要运用来解密的key：')
         choose = int(input())
-        if (choose < 1 or choose > int(len(check_key))):
+        if choose < 1 or choose > len(check_key):
             print('牛魔，参数乱填是吧')
         else:
-            choose = choose-1
+            choose = choose - 1
             return check_key[choose]
+    return args.key
 
-
+# AES解密函数
 def aes_decrypt(data, aes_key):
     try:
-        iv = b'\x00'*16
+        iv = b'\x00' * 16
         cipher = AES.new(aes_key.encode('utf-8'), AES.MODE_CBC, iv)
         decrypted = cipher.decrypt(base64.b64decode(data))
         return decrypted.rstrip(b"\0").decode('utf-8')
     except Exception as e:
         return '不是，你这key有问题啊'
 
-
+# 检查是否为JSON
 def is_json(decrypted_data):
     try:
         json.loads(decrypted_data)
+        return True
     except ValueError:
         return False
-    return True
-
 
 # 找到所有符合16位hex的值
 check_key = maybe_aeskey.findall(content)
-
 aes_key = printmaybekey(check_key)
 
+# 遍历目录，解密每个文件
+def decrypt_directory(directory, aes_key):
+    for filename in os.listdir(directory):
+        if filename.endswith('.php'):  # 检查扩展名
+            filepath = os.path.join(directory, filename)
+            with open(filepath, 'rb') as f:
+                encrypted_data = f.read().decode('utf-8')
 
-# 读取被加密的shell文件
-with open(args.decrypt) as f:
-    encrypt_file = f.read()
-    f.close
+            decrypted_data = aes_decrypt(encrypted_data, aes_key)
+            new_filename = os.path.splitext(filepath)[0] + '_decrypted.txt'
+            with open(new_filename, 'w') as f:
+                f.write(decrypted_data)
 
-decrypted_data = aes_decrypt(encrypt_file, aes_key)
-print(decrypted_data)
-newfilename = 'decrypt_file.txt'
-with open(newfilename, 'w') as f:
-    f.write(decrypted_data)
+# 根据参数处理单个文件或目录
+if args.decrypt:
+    with open(args.decrypt) as f:
+        encrypt_file = f.read()
 
-match = re.search(r'\{.*\}', decrypted_data)
-if match:
-    json_content = match.group(0)
-    try:
-        # 尝试解析JSON内容
-        data = json.loads(json_content)
+    decrypted_data = aes_decrypt(encrypt_file, aes_key)
+    newfilename = 'decrypt_file.txt'
+    with open(newfilename, 'w') as f:
+        f.write(decrypted_data)
 
-        # Base64解码
-        decoded_status = base64.b64decode(data["status"]).decode('utf-8')
-        decoded_msg = base64.b64decode(data["msg"]).decode('utf-8')
+    match = re.search(r'\{.*\}', decrypted_data)
+    if match:
+        json_content = match.group(0)
+        try:
+            data = json.loads(json_content)
+            decoded_status = base64.b64decode(data["status"]).decode('utf-8')
+            decoded_msg = base64.b64decode(data["msg"]).decode('utf-8')
+            print("Status:", decoded_status)
+            print("Msg:", decoded_msg)
+        except json.JSONDecodeError:
+            print("提取的字符串不是有效的JSON格式。")
+    else:
+        print("未找到有效的JSON格式数据。是否直接输出(Y/N)")
+        a = input()
+        if a.lower() == 'y':
+            print(decrypted_data)
 
-        print("Status:", decoded_status)
-        print("Msg:", decoded_msg)
-    except json.JSONDecodeError:
-        print("提取的字符串不是有效的JSON格式。")
-else:
-    print("未找到有效的JSON格式数据。")
+elif args.list:
+    decrypt_directory(args.list, aes_key)
